6f74c5fe9b69b69c298a944b1a3149c42d489ef6
billchen198318 <chen.xin.nien@gmail.com>
2024-10-15 21:38:38 +0800

    update

---
+++ frontend/pages/orrs001d0001/config.js
@@ -0,0 +1,7 @@
+export const PageConstants = {
+    frontendNamespace   : '/orrs001d0001',
+    eventNamespace  :   '/ORRS001D0001',
+    QueryId         :   'ORRS001D0001Q',
+    CreateId        :   'ORRS001D0001A',
+    EditId          :   'ORRS001D0001E'
+}
