ec44c3deef1819752a29c4032792d05ae78100b0
billchen198318 <chen.xin.nien@gmail.com>
2024-10-21 22:49:20 +0800

    update

--- backend/doc/orrs.sql
+++ backend/doc/orrs.sql
@@ -76,7 +76,7 @@ CREATE TABLE `tb_orrs_command` (
 
 LOCK TABLES `tb_orrs_command` WRITE;
 /*!40000 ALTER TABLE `tb_orrs_command` DISABLE KEYS */;
-INSERT INTO `tb_orrs_command` VALUES ('6fef7cd7-8e18-11ef-ad17-4132272e35db','test001','資料庫代碼','測試用的','mariadb 帳戶 root 密碼 password , 資料表 tb_sys_event_log 欄位 [OID, USER, SYS_ID, EXECUTE_EVENT] , 用 groovy 產出連線資料庫取出資料, 先將資料放入List<Map> 中,  再用com.fasterxml.jackson 將 List<Map> 內容轉成 json輸出 return 出來, 給我 groovy code','result','GROOVY','admin','2024-10-19 20:48:28','admin','2024-10-21 20:14:55');
+INSERT INTO `tb_orrs_command` VALUES ('6fef7cd7-8e18-11ef-ad17-4132272e35db','test001','資料庫代碼','測試用的','mariadb 帳戶 root 密碼 password , 資料表 tb_sys_event_log 欄位 [OID, USER, SYS_ID, EXECUTE_EVENT] OID字串格式,USER字串格式,SYS_ID字串格式,EXECUTE_EVENT字串格式 , 用 groovy 產出連線資料庫取出資料, 先將資料放入List<Map> 中,  再用com.fasterxml.jackson 將 List<Map> 內容轉成 json輸出 放到result變數中(不要宣告result), 給我 groovy code','result','GROOVY','admin','2024-10-19 20:48:28','admin','2024-10-21 22:14:18');
 /*!40000 ALTER TABLE `tb_orrs_command` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,7 +137,7 @@ CREATE TABLE `tb_orrs_command_prompt` (
 
 LOCK TABLES `tb_orrs_command_prompt` WRITE;
 /*!40000 ALTER TABLE `tb_orrs_command_prompt` DISABLE KEYS */;
-INSERT INTO `tb_orrs_command_prompt` VALUES ('151d9cca-8fa6-11ef-92b7-2b14f13fe5ba','test001',0,'jdbc driver class 為 org.mariadb.jdbc.Drive','admin','2024-10-21 20:14:55',NULL,NULL),('151f4a7b-8fa6-11ef-92b7-157862d82b95','test001',1,'json library 是 com.fasterxml.jackson','admin','2024-10-21 20:14:55',NULL,NULL),('15200dcc-8fa6-11ef-92b7-d31801b4794e','test001',2,'mariadb IP位置 127.0.0.1 , 服務 port 號是 3306','admin','2024-10-21 20:14:55',NULL,NULL),('1520d11d-8fa6-11ef-92b7-bd35b95765e6','test001',3,'資料庫檔案 orrs ','admin','2024-10-21 20:14:55',NULL,NULL);
+INSERT INTO `tb_orrs_command_prompt` VALUES ('c27015e4-8fb6-11ef-8a2d-d9c460ea1e9b','test001',0,'jdbc driver class 為 org.mariadb.jdbc.Drive','admin','2024-10-21 22:14:18',NULL,NULL),('c270b225-8fb6-11ef-8a2d-c3815c9c9bbf','test001',1,'json library 是 com.fasterxml.jackson','admin','2024-10-21 22:14:18',NULL,NULL),('c2719c86-8fb6-11ef-8a2d-091fdf92e8a7','test001',2,'mariadb IP位置 127.0.0.1 , 服務 port 號是 3306','admin','2024-10-21 22:14:18',NULL,NULL),('c27238c7-8fb6-11ef-8a2d-d9d94bd7c3c7','test001',3,'資料庫檔案 orrs ','admin','2024-10-21 22:14:18',NULL,NULL),('c2732328-8fb6-11ef-8a2d-133396090b66','test001',4,'DriverManager 建議需要 import java.sql.DriverManager','admin','2024-10-21 22:14:18',NULL,NULL);
 /*!40000 ALTER TABLE `tb_orrs_command_prompt` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,7 +170,7 @@ CREATE TABLE `tb_orrs_task` (
 
 LOCK TABLES `tb_orrs_task` WRITE;
 /*!40000 ALTER TABLE `tb_orrs_task` DISABLE KEYS */;
-INSERT INTO `tb_orrs_task` VALUES ('48c57d5e-8fa6-11ef-92b7-397a3006944e','task01','測試任務01','for test!','0 */10 * * * *','Y','admin','2024-10-21 20:16:22',NULL,NULL);
+INSERT INTO `tb_orrs_task` VALUES ('48c57d5e-8fa6-11ef-92b7-397a3006944e','task01','測試任務01','for test!','0 41 22 * * ?','Y','admin','2024-10-21 20:16:22',NULL,NULL);
 /*!40000 ALTER TABLE `tb_orrs_task` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -1102,4 +1102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-21 21:46:36
+-- Dump completed on 2024-10-21 22:48:03
