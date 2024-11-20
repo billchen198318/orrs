c278903719e1cc37e1e1569430a4935b59572a3d
billchen198318 <chen.xin.nien@gmail.com>
2024-11-20 22:41:37 +0800

    update

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <script>
-import "bootstrap/dist/css/bootstrap.css";
+//import "bootstrap/dist/css/bootstrap.css";
+import "./assets/bootswatch_bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './assets/vali.css';
 import './assets/m.css';
